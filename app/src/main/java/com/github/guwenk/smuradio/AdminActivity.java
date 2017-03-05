7be73e00bd286c3c18ae5c889c0d7e91116e35df
Stronger197 <Stronger197@gmail.com>
2017-03-05 22:44:48 +0300

    widget design update

--- app/src/main/java/com/github/guwenk/smuradio/AdminActivity.java
+++ app/src/main/java/com/github/guwenk/smuradio/AdminActivity.java
@@ -2,19 +2,26 @@ package com.github.guwenk.smuradio;
 
 import android.content.SharedPreferences;
 import android.graphics.Bitmap;
+import android.os.Build;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.Objects;
+
+import static com.github.guwenk.smuradio.R.id.etPass;
 
 
 public class AdminActivity extends AppCompatActivity {
@@ -105,7 +112,7 @@ public class AdminActivity extends AppCompatActivity {
                     }
                 });
                 try {
-                    Thread.sleep(100);
+                    Thread.sleep(1000);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
